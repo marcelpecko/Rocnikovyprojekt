b39a9c2ed1150b0008e0f9c1a5798b1c8be3e44b
marcelpecko <marcelpecko97@gmail.com>
2018-02-01 19:52:52 +0100

    registracia

--- src/Prihlasenie/index.js
+++ src/Prihlasenie/index.js
@@ -8,7 +8,7 @@ export default class Prihlasovanie extends React.Component {
       <div>
         <img src={Pozadie} className="pozadie" />
         <Form className="mojForm" style={{ position: 'relative' }}>
-          <div id="prihlasenie">  Prihlasovanie
+          <div className="prihlasenie">  <h3>Prihlasovanie </h3>
             <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
               <Label for="email" className="mr-sm-2">Email</Label>
               <Input type="email" name="email" id="email" placeholder="Email" />
@@ -19,28 +19,34 @@ export default class Prihlasovanie extends React.Component {
             </FormGroup>
             <Button>Prihlásiť</Button>
           </div>
-          <div id="registracia"> Registrácia
-            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-              <Label for="registracia_meno" className="mr-sm-2">Meno</Label>
-              <Input type="text" name="email" id="registracia_meno" placeholder="Meno" />
-            </FormGroup>
-            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-              <Label for="registracia_priezvisko" className="mr-sm-2">Priezvisko</Label>
-              <Input type="text" name="email" id="registracia_priezvisko" placeholder="Priezvisko" />
-            </FormGroup>
-            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-              <Label for="email" className="mr-sm-2">Email</Label>
-              <Input type="email" name="email" id="email" placeholder="Email" />
-            </FormGroup>
-            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-              <Label for="heslo_registracia" className="mr-sm-2">Heslo</Label>
-              <Input type="password" name="password" id="heslo_registracia" placeholder="Heslo" />
-            </FormGroup>
-            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-              <Label for="heslo_registracia_znovu" className="mr-sm-2">Zopakuj heslo</Label>
-              <Input type="password" name="password" id="heslo_registracia_znova" placeholder="Heslo" />
-            </FormGroup>
-            <Button>Registruj</Button>
+
+          <div className="toggle">
+            <div className="toggle-header">
+              <h3>  Registrácia </h3>
+              <div className="toggle-registracia">
+                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                  <Label for="registracia_meno" className="mr-sm-2">Meno</Label>
+                  <Input type="text" name="email" id="registracia_meno" placeholder="Meno" />
+                </FormGroup>
+                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                  <Label for="registracia_priezvisko" className="mr-sm-2">Priezvisko</Label>
+                  <Input type="text" name="email" id="registracia_priezvisko" placeholder="Priezvisko" />
+                </FormGroup>
+                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                  <Label for="email" className="mr-sm-2">Email</Label>
+                  <Input type="email" name="email" id="email" placeholder="Email" />
+                </FormGroup>
+                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                  <Label for="heslo_registracia" className="mr-sm-2">Heslo</Label>
+                  <Input type="password" name="password" id="heslo_registracia" placeholder="Heslo" />
+                </FormGroup>
+                <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                  <Label for="heslo_registracia_znovu" className="mr-sm-2">Zopakuj heslo</Label>
+                  <Input type="password" name="password" id="heslo_registracia_znova" placeholder="Heslo" />
+                </FormGroup>
+                <Button>Registruj</Button>
+              </div>
+            </div>
           </div>
         </Form>
       </div>
