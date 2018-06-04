c462e18be4368512ce8d994a79c9bf7178eb67dc
marcelpecko <marcelpecko97@gmail.com>
2018-06-04 23:51:10 +0200

    Remove previous frontend

--- prev_front/src/initialState.js
+++
@@ -1,8 +0,0 @@
-import {setInitialState as appInitialState} from './App/state.js'
-import {compose} from './utils/helpers'
-
-const state = {}
-
-export default () => compose(
-  appInitialState,
-)(state)
--- prev_front/webpack/config.main.babel.js
+++
@@ -1,6 +0,0 @@
-import baseConfig from './config.base.js'
-import merge from 'webpack-merge'
-
-console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`)
-console.log(`Using config: ./config.${process.env.NODE_ENV}.js`)
-export default merge({}, baseConfig, require(`./config.${process.env.NODE_ENV}.js`).default)
